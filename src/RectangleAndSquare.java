e15d2522e544f6dbf5336170e5d2e11e514da12e
spidey-mony <110289642+spidey-mony@users.noreply.github.com>
2024-01-11 08:47:32 +0700

    for Midterm Exam

---
+++ src/RectangleAndSquare.java
@@ -0,0 +1,12 @@
+public class RectangleAndSquare {
+    public static void main(String[] args) {
+
+        Square square = new Square(10);
+
+    double area = square.getArea();
+    double perimeter = square.getPerimeter();
+
+    System.out.println("Square Area: " + area);
+    System.out.println("Square Perimeter: " + perimeter);
+    }
+}
--- src/dateConversion.java
+++ src/dateConversion.java
@@ -1,11 +1,23 @@
 import java.util.Scanner;
+import java.text.DateFormatSymbols;
 
 public class dateConversion {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Short Date(YYYY-MM-DD): ");
-        String date = input.next();
+        String shortDateFormat = input.nextLine();
+
+        String[] datePart = shortDateFormat.split("-");
+        int day = Integer.parseInt(datePart[2]);
+        int month = Integer.parseInt(datePart[1]);
+        int year = Integer.parseInt(datePart[0]);
+
+        String monthOutput = new DateFormatSymbols().getMonths()[month - 1 ];
+        System.out.println("Output : " + monthOutput + " " + day + "," + year);
+
+        input.close();
+
 
     }
 }
