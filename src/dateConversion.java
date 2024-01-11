ec8a470da7fba239de31b8d13437fce4f1160401
spidey-mony <110289642+spidey-mony@users.noreply.github.com>
2024-01-11 08:17:54 +0700

    Midterm Exam

---
+++ src/dateConversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class dateConversion {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Short Date(YYYY-MM-DD): ");
+        String date = input.next();
+
+    }
+}
